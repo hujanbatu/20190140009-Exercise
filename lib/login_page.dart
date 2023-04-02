5069fdd7f4ce76307149d0fb261d42d29d9c5507
hujanbatu <haikal04102001p@gmail.com>
2023-04-02 10:59:43 +0700

    Login Page Layout

---
+++ lib/login_page.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _name = '';
+  String _password = '';
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.all(24.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/img/pokemon_logo.png',
+                      height: 400.0,
+                    ),
+                    Text(
+                      "Let's get started.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40.0,
+                        color: Colors.amber,
+                      ),
+                    ),
+                    SizedBox(height: 40.0),
+                    Text(
+                      " There's No Sense In Going Out Of\nYour Way To Get Somebody To Like You.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    SizedBox(height: 40.0),
+                    TextFormField(
+                      onChanged: (value) {
+                        setState(() {
+                          _name = value;
+                        });
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Name',
+                        prefixIcon: Icon(Icons.person),
+                        border: UnderlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                    TextFormField(
+                      obscureText: _obscureText,
+                      onChanged: (value) {
+                        setState(() {
+                          _password = value;
+                        });
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        prefixIcon: Icon(Icons.lock),
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _obscureText = !_obscureText;
+                            });
+                          },
+                          icon: Icon(
+                            _obscureText
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        border: UnderlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(height: 24.0),
+                    ElevatedButton(
+                      onPressed: () {
+                        // if (_formKey.currentState.validate()) {
+                        //   // TODO: Sign in logic here
+                        // }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        primary: Colors.amber,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Text(
+                          'Sign in',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Don\'t have an account? '),
+                        TextButton(
+                          onPressed: () {
+                            // TODO: Navigate to sign up page here
+                          },
+                          child: Text(
+                            'Sign up',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.amber,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,64 @@
 import 'package:flutter/material.dart';
+import 'login_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-      ),
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Pokedex.",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 60.0,
-                  color: Colors.amber,
-                ),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Pokedex.",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 60.0,
+                color: Colors.amber,
               ),
-              SizedBox(height: 40.0),
-              Text(
-                "Welcome to Pokedex! \nYou can find your favorite pokemon here",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  color: Colors.grey,
-                ),
+            ),
+            SizedBox(height: 40.0),
+            Text(
+              "Welcome to Pokedex! \nYou can find your favorite pokemon here",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 25.0,
+                color: Colors.grey,
               ),
-              SizedBox(height: 200.0),
-              Image.asset(
-                'assets/img/pokedex_logo.png',
-                height: 200.0,
+            ),
+            SizedBox(height: 200.0),
+            Image.asset(
+              'assets/img/pokedex_logo.png',
+              height: 200.0,
+            ),
+            SizedBox(height: 150.0),
+            Container(
+              width: 60.0,
+              height: 60.0,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.amber,
               ),
-              SizedBox(height: 150.0),
-              Container(
-                width: 60.0,
-                height: 60.0,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.amber,
+              child: IconButton(
+                icon: Icon(
+                  Icons.arrow_forward,
+                  color: Colors.white,
                 ),
-                child: IconButton(
-                  icon: Icon(
-                    Icons.arrow_forward,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {
-                    // Navigator.push(
-                    //   context,
-                    //   MaterialPageRoute(builder: (context) => LoginPage()),
-                    // );
-                  },
-                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginPage()),
+                  );
+                },
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
